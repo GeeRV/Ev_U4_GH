35d39f12f9d30b06b8761dede0521dfe46a86884
GeeRV <96028707+GeeRV@users.noreply.github.com>
2021-12-12 18:06:03 -0600

    Add files via upload

---
+++ Empresa.py
@@ -0,0 +1,20 @@
+"Rodríguez Vázquez Gerardo"
+"Evaluación Unidad 4"
+"Programación Avanzada"
+
+
+
+"""Este archivo unicamente se utiliza para dar información de la empresa...
+Solo se requiere en caso de colocar el metodo de la clase
+Cuenta como archivo adicional
+"""
+
+class Company:
+    def __init__(self, Nombre, Direccion, Vacante):
+        self.__Nombre=Nombre
+        self.__Direccion=Direccion
+        self.__Vacante=Vacante
+    def GetInfoEmpresa(self):
+        print("El nombre de la empresa es:", self.__Nombre)
+        print("La direccion es:", self.__Direccion)
+        print("Tipo de vacante:", self.__Vacante)
---
+++ Evaluacion.py
@@ -0,0 +1,27 @@
+"Rodríguez Vázquez Gerardo"
+"Evaluación Unidad 4"
+"Programación Avanzada"
+
+
+import Personal
+import Empresa
+empresa=Empresa.Company("TraditionalStart", "Zona centro, Calle Alfonzo Luna No.32", "Puesto de empleado")
+empresa.GetInfoEmpresa()
+Nombre=str(input("Ingrese su nombre: "))
+Altura=float(input("Ingrese su altura: "))
+Vestimenta=str(input("Ingrese su vestimenta: "))
+print("Seleccione su puesto dependiendo el numero:")
+print(" 1.-Gerente.\n","2.-Empleado.\n","3.-Persona.")
+Puesto=int(input("Numero: "))
+if Puesto == 1:
+    gerente=Personal.Gerente(Nombre,Altura,Vestimenta)
+    gerente.status()
+    gerente.Entrevistando()
+elif Puesto == 2:
+    empleado=Personal.Empleado(Nombre,Altura,Vestimenta)
+    empleado.status()
+    empleado.Trabajanto()
+elif Puesto == 3:
+    persona=Personal.Persona(Nombre,Altura,Vestimenta)
+    persona.status()
+    persona.Solicitando()
